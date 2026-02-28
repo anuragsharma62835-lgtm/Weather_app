cbc5f8af21a68c3c83a349a3aa7b176ff558f59e
Anurag Sharma <anuragsharma62835@gmail.com>
2026-02-28 19:34:53 +0530

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
@@ -10,7 +10,7 @@ function App() {
   async function getWeather() {
     try {
       const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`,
       );
       setWeather(res.data);
     } catch (error) {
@@ -33,15 +33,30 @@ function App() {
 
           {weather ? (
             <div>
-              <h2>name: {weather.name}</h2>
-              <p>Temp: {weather.main.temp} C</p>
-              <p>sky: {weather.weather[0].description}</p>
-              <p>description: {weather.weather[0].main}</p>
+              <h2>
+                {weather.name}, {weather.sys.country}
+              </h2>
+
+              <h2>{weather.main.temp} °C</h2>
+              <p>{weather.weather[0].description}</p>
+
+              <p>Feels Like: {weather.main.feels_like} °C</p>
+              <p>Humidity: {weather.main.humidity}%</p>
+              <p>Pressure: {weather.main.pressure} hPa</p>
+              <p>Wind Speed: {weather.wind.speed} m/s</p>
+
+              <p>
+                Sunrise:{" "}
+                {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()}
+              </p>
+
+              <p>
+                Sunset:{" "}
+                {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}
+              </p>
             </div>
           ) : (
-            <div>
-              <h2>enter city name...</h2>
-            </div>
+            <h2>Enter city name...</h2>
           )}
         </div>
       </div>
